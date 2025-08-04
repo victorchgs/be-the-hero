6f599acd76e40a09ad94774bd4ada76f7d644cf0
Víctor Chagas <victorchagaslm@gmail.com>
2025-08-04 19:50:25 -0300

    feature: implementando app mobile

---
+++ mobile/types/incident.ts
@@ -0,0 +1,12 @@
+export type Incident = {
+  id: number;
+  title: string;
+  description: string;
+  value: number;
+  ong_id: string;
+  name: string;
+  email: string;
+  whatsapp: string;
+  city: string;
+  uf: string;
+};
